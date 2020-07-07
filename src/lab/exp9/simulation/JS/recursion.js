f637da6c5bd4641348326d98a165da05d68711ad
Saumya <saumya.1126@gmail.com>
2020-07-07 12:29:36 +0530

    Fixed issue CSE9

--- src/lab/exp9/simulation/JS/recursion.js
+++ src/lab/exp9/simulation/JS/recursion.js
@@ -19,12 +19,12 @@ window.view = {
 	},
 	getInput: function() {
 		 var input = Number(document.getElementById('input').value)
-		 if ( input === 0 )
-		 	alert('Enter number of disks first !')
-		 else if ( isNaN(input) === true )
+	/*	 if ( input === 0 )
+		 	alert('Enter number of disks first !') */
+		 if ( isNaN(input) === true )
 			alert('Number of disks must be an integer value !')
-		 else if ( input > 5 || input % 1 !== 0 )
-			alert('Only integral value (Greater than equal to 1 and less than equal to 5) is accepted !')
+		 else if ( input > 10 || input % 1 !== 0 )
+			alert('Invalid Input-Only integral value (Greater than equal to 0 and less than equal to 10) is accepted !')
 		 else {
 			this.disableElement('btnOk')
 			this.enableElement('btnStart')
